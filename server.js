45eb8db418ccb1c71b9e6bdd934d0ab4bdcbd689
Aaron Bartholomew <aaron@dzap.net>
2015-09-20 22:43:05 -0700

    Updated server with http headers

--- server.js
+++ server.js
@@ -14,10 +14,12 @@ app.get('/', function(req, res){
 });
 /* REVIEWS ROUTE LISTENER */
 app.get('/reviews', function(req, res){
-	plat = req.query.p ? req.query.p : platforms[0];
-	url = "http://www.metacritic.com/browse/games/release-date/available/"+plat+"/date";
+	plat = req.query.plat ? req.query.plat : platforms[0];
+	page = req.query.p ? req.query.p : 0;
+	url = "http://www.metacritic.com/browse/games/release-date/available/"+plat+"/date?page="+page;
+	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'};
 	console.log("Scrape Request initiated: "+url);
-	request(url, function(error, response, html){
+	request({url:url, headers: headers}, function(error, response, html){
 			console.log("Successfully retrieved data from "+url);
 			if(!error){
 					var $ = cheerio.load(html);
@@ -26,14 +28,19 @@ app.get('/reviews', function(req, res){
 
 					if($('ol.list_products .game_product').length > 0) {
 						$('ol.list_products .game_product').each(function(i, ele) {
+							metascore = ($(this).find(".metascore_w").text().trim()=="tbd") ? false : true;
+							userscore = ($(this).find(".product_avguserscore").text().split(":")[1].trim()=="tbd") ? false : true;
 							title = slug($(this).find(".product_title a").text().trim()).toLowerCase();
-							output.games[title] = {};
-							//output.games[i].html = $(this).html();
-							output.games[title].title = $(this).find(".product_title a").text().trim();
-							output.games[title].slug = title;
-							output.games[title].meta_score = $(this).find(".metascore_w").text().trim();
-							output.games[title].user_score = $(this).find(".product_avguserscore").text().split(":")[1].trim();
-							output.games[title].html = $(this).html();
+							if(userscore || metascore) {
+								output.games[title] = {};
+								//output.games[i].html = $(this).html();
+								output.games[title].title = $(this).find(".product_title a").text().trim();
+								output.games[title].slug = title;
+								output.games[title].meta_score = $(this).find(".metascore_w").text().trim();
+								output.games[title].user_score = $(this).find(".product_avguserscore").text().split(":")[1].trim();
+								//output.games[title].release_data = $(this).find(".release_date .data").text().trim();
+								//output.games[title].html = $(this).html();
+							}
 						});				
 					} else {
 						output.html = html;
