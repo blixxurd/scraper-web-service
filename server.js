8445dbb2784392863233f860b7db1f22134b917d
Aaron Bartholomew <abartholomew@blixhost.com>
2017-06-25 15:11:34 -0700

    Adds gitignore. Updates URL Param checking

--- server.js
+++ server.js
@@ -8,45 +8,49 @@ var app     	= 	express();
 app.get('/', function(req, res){
 
 
-	url = req.query.url ? req.query.url : "http://google.com/";
-	console.log("Scrape Request initiated: "+url);
-	request(url, function(error, response, html){
-			console.log("Successfully retrieved data from "+url);
-			if(!error){
-					var $ = cheerio.load(html);
+	url = req.query.url ? req.query.url : null;
+	if(url) {
+		console.log("Scrape Request initiated: "+url);
+		request(url, function(error, response, html){
+				console.log("Successfully retrieved data from "+url);
+				if(!error){
+						var $ = cheerio.load(html);
 
-					var output = {}, rawScript;
+						var output = {}, rawScript;
 
-					//Build Output
-					output.request_url = url;
-					output.assets= {};
-					output.scripts= {};
-					output.images= {};
-					output.links = {};
-					output.dom_object= html;
+						//Build Output
+						output.request_url = url;
+						output.assets= {};
+						output.scripts= {};
+						output.images= {};
+						output.links = {};
+						output.raw_html = html;
 
-					$('link').each(function(i, ele) {
-						output.assets[i] = $(this).attr('href');
-					});
+						$('link').each(function(i, ele) {
+							output.assets[i] = $(this).attr('href');
+						});
 
 
-					$('a').each(function(i, ele) {
-						output.links[i] = $(this).attr('href');
-					});
+						$('a').each(function(i, ele) {
+							output.links[i] = $(this).attr('href');
+						});
 
-					$('script').each(function(i, ele) {
-						output.scripts[i] = $(this).attr('src');
-					});
+						$('script').each(function(i, ele) {
+							output.scripts[i] = $(this).attr('src');
+						});
 
-					$('img').each(function(i, ele) {
-						output.images[i] = $(this).attr('src');
-					});
+						$('img').each(function(i, ele) {
+							output.images[i] = $(this).attr('src');
+						});
 
-			}
-			console.log("Outputting Scrape Data for "+url);
-			res.json(output);
+				}
+				console.log("Outputting Scrape Data for "+url);
+				res.json(output);
 
-	});
+		});
+	} else {
+		res.json({"error": "URL Param not defined."});
+	}
 
 });
 
